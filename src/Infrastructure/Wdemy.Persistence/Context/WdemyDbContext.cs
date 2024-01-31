5444f4c7677259facbea5bddcdc95202bd5c1a8f
yasinn gürbüz <yasingurbuz1@hotmail.com>
2024-01-31 15:34:02 +0300

    .

--- src/Infrastructure/Wdemy.Persistence/Context/WdemyDbContext.cs
+++ src/Infrastructure/Wdemy.Persistence/Context/WdemyDbContext.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Wdemy.Domain.Entities;
 
 namespace Wdemy.Persistence.Context
 {
-    public class WdemyDbContext
+    public class WdemyDbContext : DbContext
     {
+        public WdemyDbContext(DbContextOptions<WdemyDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Admin> Admins => Set<Admin>();
+        public DbSet<Trainer> Trainers => Set<Trainer>();
+        public DbSet<Student> Students => Set<Student>();
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<SubCategory> SubCategories => Set<SubCategory>();
+        public DbSet<Subject> Subjects => Set<Subject>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+
     }
 }
