3b1d939936eb33fd76aa079895dc4e9696eff307
yasinn gürbüz <yasingurbuz1@hotmail.com>
2024-02-02 01:12:21 +0300

    >

--- src/Core/Wdemy.Application/Constant/AuthorizationConstants.cs
+++ src/Core/Wdemy.Application/Constant/AuthorizationConstants.cs
@@ -9,6 +9,8 @@ namespace Wdemy.Application.Constant
     public static class AuthorizationConstants
     {
         public const string DEFAULT_ADMIN_USER = "mesutkilic@tureb.com";
+        public const string DEFAULT_TRAINER_USER = "mesutkilic@tureb.com";
+        public const string DEFAULT_STUDENT_USER = "mesutkilic@tureb.com";
         public const string DEFAULT_PASSWORD = "Tureb123";
 
         public static class Roles
