848c1e8e1d025b51426eb9f36294e2412ffbe70a
yasinn gürbüz <yasingurbuz1@hotmail.com>
2024-02-02 23:36:37 +0300

    .

---
+++ src/Web/Wdemy.Mvc/Areas/Trainer/Controllers/CourseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Wdemy.Mvc.Areas.Trainer.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
