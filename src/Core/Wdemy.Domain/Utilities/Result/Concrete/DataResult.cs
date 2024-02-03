79d5dd05918b8f024799724d77e64a5ae129da6d
yasinn gürbüz <yasingurbuz1@hotmail.com>
2024-02-04 01:19:52 +0300

    >

--- src/Core/Wdemy.Domain/Utilities/Result/Concrete/DataResult.cs
+++ src/Core/Wdemy.Domain/Utilities/Result/Concrete/DataResult.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace Wdemy.Domain.Utilities.Result.Concrete
 {
-    public class DataResult
+    public class DataResult<T> : Result, IDataResult<T> where T : class
     {
+        public T Data { get; }
+        public DataResult(T data, bool isSuccess) : base(isSuccess) => Data = data;
+        public DataResult(T data, bool isSuccess, string message) : base(isSuccess, message) => Data = data;
     }
 }
---
+++ src/Core/Wdemy.Domain/Utilities/Result/Concrete/ErrorResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wdemy.Domain.Utilities.Result.Concrete
+{
+    public class ErrorResult : Result
+    {
+        public ErrorResult() : base(false)
+        { }
+        public ErrorResult(string message) : base(false, message)
+        { }
+    }
+}
