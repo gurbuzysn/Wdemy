21bb169cd9a869fa45ce0d4e60578b40c82dc10c
yasinn gürbüz <yasingurbuz1@hotmail.com>
2024-02-11 01:02:14 +0300

    Video section was created

--- src/Web/Wdemy.Mvc/wwwroot/js/get-video.js
+++ src/Web/Wdemy.Mvc/wwwroot/js/get-video.js
@@ -1,9 +1,11 @@
-﻿    function openVideo(videoUri){
+﻿function openVideo(videoUri) {
 
-    console.log(videoUri)
+    let videoPath = "/videos/" + videoUri;
+    let videoPlayer = document.getElementById('videoPlayer');
+    videoPlayer.src = videoPath;
+    videoPlayer.play();
+}
 
-    }
-        
 
 
 
